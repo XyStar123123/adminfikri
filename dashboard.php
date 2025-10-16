69f997b3b87114b8010877d8938bbbb3738cb00f
fikriaja1028 <fikriamazing5757@gmail.com>
2025-10-16 17:14:42 +0700

    gk penting

--- dashboard.php
+++ dashboard.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style></style>
 </head>
 <body>
-    <div class="container"> </div>
+    <div class="container">
+        <header>
+            <nav></nav>
+        </header>
+    </div>
 </body>
 </html>
