034b2f8db79e92fcbc7d8c80c2ecad2cc1b385dd
XyStar123123 <hermawanbintang20@gmail.com>
2025-10-17 07:05:34 +0700

    Added bunch of content in about page

--- about.php
+++ about.php
@@ -4,12 +4,91 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="about.css">
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
     <div class="container">
-        <section class="version-section">
-            
+        <section class="app-info-section">
+            <h2 class="app-info-header">
+                <i data-lucide="info"></i>
+                <p>About Apps</p>
+            </h2>
+
+            <div class="info-container">
+                <div class="info-divider">
+                    <i data-lucide="user"></i>
+                    <span class="app-header">Name:</span>
+                    <span class="app-name">Admin Fikri</span>
+                </div>
+
+                <div class="info-divider">
+                    <i data-lucide="hash"></i>
+                    <span class="version-header">Version:</span>
+                    <span class="version-app">1.0.0</span>
+                </div>
+
+                <div class="info-divider">
+                    <i data-lucide="clock"></i>
+                    <span class="uptime-header">Uptime:</span>
+                    <span class="uptime-app">60ms</span>
+                </div>
+
+                <div class="info-divider">
+                    <i data-lucide="calendar-days"></i>
+                    <span class="last-update-header">Last Update:</span>
+                    <span class="last-update-app">Friday, 24 October 2025</span>
+                </div>
+
+                <div class="info-divider">
+                    <i data-lucide="info"></i>
+                    <span class="description-header">Description:</span>
+                    <span class="description-app">
+                    Admin Fikri adalah aplikasi khusus admin untuk mengelola data data pada suatu organisasi agar lebih cepat dan efisien
+                    </span>
+                </div>
+
+                <div class="info-divider">
+                    <i data-lucide="activity"></i>
+                    <span class="status-system-header">Status System:</span>
+                    <span class="status-system-app">Online</span>
+                </div>
+            </div>
+        </section>
+        <section class="tech-stack-section">
+            <h2 class="tech-stack-header">
+                <i data-lucide="brain-circuit"></i>
+                <p>Tech Stack</p>
+            </h2>
+            <div class="tech-stack-container">
+                <div class="tech-card">
+                    <i class="bx bxl-html5 icon-tech"></i>
+                    <h3>HTML</h3>
+                </div>
+                <div class="tech-card">
+                    <i class="bx bxl-css3 icon-tech"></i>
+                    <h3>CSS</h3>
+                </div>
+                <div class="tech-card">
+                    <i class="bx bxl-javascript icon-tech"></i>
+                    <h3>JavaScript</h3>
+                </div>
+                <div class="tech-card">
+                    <i class="bx bxl-php icon-tech"></i>
+                    <h3>PHP</h3>
+                </div>
+                <div class="tech-card">
+                    <i class="bx bx-data icon-tech"></i>
+                    <h3>MySQL</h3>
+                </div>
+            </div>
         </section>
     </div>
+<script src="https://unpkg.com/lucide@latest"></script>
+<script>
+    lucide.createIcons()
+</script>
 </body>
 </html>
