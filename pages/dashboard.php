764128b4ebe477585e97839b6abda860159db492
fikriaja1028 <fikriamazing5757@gmail.com>
2025-10-18 20:41:43 +0700

    Changes at dashboard.php

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -12,9 +12,9 @@
             /* overflow: hidden; */
         }
         .sidebar {
-            margin-left: 8px;
-            width: 300px;
-            border-right: 1px solid rgba(0,0,0,0.05);
+            margin-left: 0px;
+            width: 195px;
+            border-right: 1px solid rgba(59, 118, 209, .5);
             height: 100vh;
             padding: 20px;
         }
@@ -30,7 +30,7 @@
         #admin {
             color: black;
             text-decoration: none;
-            font-size: 1.7rem;
+            font-size: 1.2rem;
             font-weight: bold;
             transition: color 2s ease;
         }
@@ -46,22 +46,36 @@
         #admin:hover #adminSpan {
             color: black;
         }
+        nav {
+            margin-top: 20px;
+        }
         .nav_a {
             display: block;
             padding: 10px 10px;
             text-decoration: none;
-            font-size: 1.2rem;
+            font-size: 1rem;
             color: #6b7280;
             font-weight: bold;
             transition: all .4s ease;
+            margin-top: 10px;
+            /* border: 1px solid black; */
         }
         .nav_a:hover {
             color: black;
+            box-shadow: 0 2px 6px rgba(0, 0, 0, .2);
+        }
+
+        .nav_a:active {
+            box-shadow: inset 0 2px 6px rgba(0, 0, 0, .2);
         }
 
         footer {
             background-color: green;
             margin: 0;
+        }
+        main {
+            padding: 10px;
+            background-color: #f5f7fb;
         }
     </style>
 </head>
@@ -69,7 +83,7 @@
     <div style="display: flex;">
     <aside class="sidebar">
         <div class="logo-container">
-            <img src="../img/icon.png" class="icon">
+            <img src="../img/icon.png" class="icon" style="width: 60px;">
             <a href="about.php" id="admin">Admin<span id="adminSpan">Panel<span></a>
         </div>
         <nav>
@@ -77,10 +91,13 @@
             <a href="#" class="nav_a">Tables</a>
             <a href="#" class="nav_a">Settings</a>
             <a href="about.php" class="nav_a">About</a>
+            <a href="contact.php" class="nav_a">Contact</a>
         </nav>
     </aside>
     <main>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe sunt fugit maiores, sint tempora, eligendi maxime sit harum, delectus non quis laudantium velit vel ipsam! Cupiditate debitis fugit facilis id sunt quia nisi fugiat provident ea impedit voluptatibus sequi, odit deleniti neque harum ut tenetur? Error blanditiis dolorum quaerat. Soluta unde, accusamus voluptates temporibus et, reiciendis impedit facilis sequi sed officia quam, quasi vitae maiores velit facere iure eligendi eaque? Nihil ex vel quaerat harum quia laboriosam voluptates quos modi ab voluptatem odit, placeat, ea, ipsam dignissimos atque doloremque temporibus unde quae minima possimus distinctio esse. Necessitatibus obcaecati quod quo?</p>
+        <iframe id="13996405" allowtransparency="true" frameborder="0" style="height: 70vh; width: 100%; border:none;" src="https://www.chess.com/emboard?id=13996405"></iframe><script nonce="chesscom-diagram">window.addEventListener("message",e=>{e['data']&&"13996405"===e['data']['id']&&document.getElementById(`${e['data']['id']}`)&&(document.getElementById(`${e['data']['id']}`).style.height=`${e['data']['frameHeight']+37}px`)});</script>
+        <iframe id="13996417" allowtransparency="true" frameborder="0" style="height: 70vh; width: 100%; border:none;" src="https://www.chess.com/emboard?id=13996417"></iframe><script nonce="chesscom-diagram">window.addEventListener("message",e=>{e['data']&&"13996417"===e['data']['id']&&document.getElementById(`${e['data']['id']}`)&&(document.getElementById(`${e['data']['id']}`).style.height=`${e['data']['frameHeight']+37}px`)});</script>
+        <iframe id="13996455" allowtransparency="true" frameborder="0" style="height: 70vh; width: 100%; border:none;" src="https://www.chess.com/emboard?id=13996455"></iframe><script nonce="chesscom-diagram">window.addEventListener("message",e=>{e['data']&&"13996455"===e['data']['id']&&document.getElementById(`${e['data']['id']}`)&&(document.getElementById(`${e['data']['id']}`).style.height=`${e['data']['frameHeight']+37}px`)});</script>
     </main>
     </div>
 </body>
