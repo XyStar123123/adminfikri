6485b19b9e8cb1134324ea0c47423a8e40416e77
XyStar123123 <hermawanbintang20@gmail.com>
2025-10-18 18:39:19 +0700

    Added contact page

---
+++ pages/contact.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/contact.css">
+</head>
+<body>
+    <div class="container">
+        <div class="contact-container">
+            <div class="contact-form">
+                <form action="">
+                    <h2>Contact Form</h2>
+                </form>
+            </div>
+            <div class="contact-information"></div>
+        </div>
+    </div>
+</body>
+</html>
