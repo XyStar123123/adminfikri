ee75c633a864895901a7d6f61d5161692f7ba87e
fikriaja1028 <fikriamazing5757@gmail.com>
2025-10-18 08:21:16 +0700

    Dashboard.php

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -15,14 +15,48 @@
             display: flex;
             width: 5rem;
         }
+        .logo-container {
+            display: flex;
+            align-items: center;
+            gap: 10px;
+        }
+        #admin {
+            color: black;
+            text-decoration: none;
+            font-size: 1.7rem;
+            font-weight: bold;
+        }
+        #adminSpan {
+            color: #3b76d1;
+            font-weight: bolder;
+        }
+
+        #admin:hover {
+            color:  #3b76d1;
+            transition: color 2s ease;    
+        }
+        #admin:hover #adminSpan {
+            color: black;
+            transition: color 1.1s ease;
+        }
     </style>
 </head>
 <body>
     <aside class="sidebar">
-        <div>
+        <div class="logo-container">
             <img src="../img/icon.png" class="icon">
-            <p style="display: inline;">Admin Panel</p>
+            <a href="about.php" id="admin">Admin<span id="adminSpan">Panel<span></a>
         </div>
+        <nav>
+            <ul>
+                <li>Dashboard</li>
+                <li>About</li>
+            </ul>
+        </nav>
+        <button>Dark Mode</button>
     </aside>
+    <footer>
+
+    </footer>
 </body>
 </html>
