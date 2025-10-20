a4aef9375317408263f80d70b18d17c6dd271538
XyStar123123 <hermawanbintang20@gmail.com>
2025-10-20 15:15:41 +0700

    Added navbar to contact.php

---
+++ folder/navbar.php
@@ -0,0 +1,26 @@
+<nav class="navbar">
+    <div class="search-container">
+        <i data-lucide="search"></i>
+        <input type="text">
+    </div>
+    <ul class="nav-link-container">
+        <li class="nav-item">
+            <a href="" class="nav-link">
+                <i data-lucide="github"></i>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="" class="nav-link">
+                <i data-lucide="bell"></i>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="" class="nav-link">
+                <i data-lucide="sun"></i>
+            </a>
+        </li>
+        <li class="profile">
+            <img src="../img/profile.png" alt="">
+        </li>
+    </ul>
+</nav>
--- pages/contact.php
+++ pages/contact.php
@@ -3,23 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="website icon" href="../img/icon.png">
+    <title>Admin Panel - Contact</title>
     <link rel="stylesheet" href="../css/main.css">
-    <link rel="stylesheet" href="../css/contact.css">
+    <link rel="stylesheet" href="../css/about.css">
+    <link rel="website icon" href="../img/icon.png">
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <?php include '../folder/sidebar.php'; ?>
     <div class="container">
-        <div class="contact-container">
-            <div class="contact-form">
-                <form action="">
-                    <h2>Contact Form</h2>
-                </form>
-            </div>
-            <div class="contact-information"></div>
+        <?php include '../folder/sidebar.php'; ?>
         </div>
+        <main class="content">
+            <?php include_once '../folder/navbar.php' ?>
+            <div class="contact-container">
+                <div class="contact-form">
+                    <form action="">
+                        <h2>Contact Form</h2>
+                    </form>
+                </div>
+                <div class="contact-information"></div>
+            </div>
+        </main>
     </div>
+<script src="https://unpkg.com/lucide@latest"></script>
+<script>
+    lucide.createIcons()
+</script>
+<script src="../js/index.js" type="module"></script>
 </div>
 </body>
 </html>
