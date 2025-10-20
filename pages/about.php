5cd66ddfb3f0dff353ad54d9edcdc0b69d40b2ff
XyStar123123 <hermawanbintang20@gmail.com>
2025-10-20 13:25:41 +0700

    Seperating header from container

--- pages/about.php
+++ pages/about.php
@@ -12,80 +12,82 @@
 </head>
 <body>
     <div class="container">
-        <section class="app-info-section">
-            <h2 class="app-info-header">
-                <i data-lucide="info"></i>
-                <p>About Apps</p>
-            </h2>
-
-            <div class="info-container">
-                <div class="info-divider">
-                    <i data-lucide="user"></i>
-                    <span class="app-header">Name:</span>
-                    <span class="app-name">Admin Fikri</span>
-                </div>
-
-                <div class="info-divider">
-                    <i data-lucide="hash"></i>
-                    <span class="version-header">Version:</span>
-                    <span class="version-app">1.0.0</span>
-                </div>
-
-                <div class="info-divider">
-                    <i data-lucide="clock"></i>
-                    <span class="uptime-header">Uptime:</span>
-                    <span class="uptime-app">60ms</span>
-                </div>
-
-                <div class="info-divider">
-                    <i data-lucide="calendar-days"></i>
-                    <span class="last-update-header">Last Update:</span>
-                    <span class="last-update-app">Friday, 24 October 2025</span>
-                </div>
-
-                <div class="info-divider">
+        <?php include_once '../folder/sidebar.php' ?>
+        <main class="content">
+            <section class="app-info-section">
+                <h2 class="app-info-header">
                     <i data-lucide="info"></i>
-                    <span class="description-header">Description:</span>
-                    <span class="description-app">
-                    Admin Fikri adalah Sebuah aplikasi anomali khusus admin untuk mengelola data data pada suatu organisasi agar lebih cepat dan efisien
-                    </span>
-                </div>
+                    <p>About Apps</p>
+                </h2>
+                <div class="info-container">
+                    <div class="info-divider">
+                        <i data-lucide="user"></i>
+                        <span class="app-header">Name:</span>
+                        <span class="app-name">Admin Fikri</span>
+                    </div>
 
-                <div class="info-divider">
-                    <i data-lucide="activity"></i>
-                    <span class="status-system-header">Status System:</span>
-                    <span class="status-system-app">Online</span>
+                    <div class="info-divider">
+                        <i data-lucide="hash"></i>
+                        <span class="version-header">Version:</span>
+                        <span class="version-app">1.0.0</span>
+                    </div>
+
+                    <div class="info-divider">
+                        <i data-lucide="clock"></i>
+                        <span class="uptime-header">Uptime:</span>
+                        <span class="uptime-app">60ms</span>
+                    </div>
+
+                    <div class="info-divider">
+                        <i data-lucide="calendar-days"></i>
+                        <span class="last-update-header">Last Update:</span>
+                        <span class="last-update-app">Friday, 24 October 2025</span>
+                    </div>
+
+                    <div class="info-divider">
+                        <i data-lucide="info"></i>
+                        <span class="description-header">Description:</span>
+                        <span class="description-app">
+                        Admin Fikri adalah Sebuah aplikasi anomali khusus admin untuk mengelola data data pada suatu organisasi agar lebih cepat dan efisien
+                        </span>
+                    </div>
+
+                    <div class="info-divider">
+                        <i data-lucide="activity"></i>
+                        <span class="status-system-header">Status System:</span>
+                        <span class="status-system-app">Online</span>
+                    </div>
                 </div>
-            </div>
-        </section>
-        <section class="tech-stack-section">
-            <h2 class="tech-stack-header">
-                <i data-lucide="brain-circuit"></i>
-                <p>Tech Stack</p>
-            </h2>
-            <div class="tech-stack-container">
-                <div class="tech-card">
-                    <i class="bx bxl-html5 icon-tech"></i>
-                    <h3>HTML</h3>
+            </section>
+            <section class="tech-stack-section">
+                <h2 class="tech-stack-header">
+                    <i data-lucide="brain-circuit"></i>
+                    <p>Tech Stack</p>
+                </h2>
+                <div class="tech-stack-container">
+                    <div class="tech-card">
+                        <i class="bx bxl-html5 icon-tech"></i>
+                        <h3>HTML</h3>
+                    </div>
+                    <div class="tech-card">
+                        <i class="bx bxl-css3 icon-tech"></i>
+                        <h3>CSS</h3>
+                    </div>
+                    <div class="tech-card">
+                        <i class="bx bxl-javascript icon-tech"></i>
+                        <h3>JavaScript</h3>
+                    </div>
+                    <div class="tech-card">
+                        <i class="bx bxl-php icon-tech"></i>
+                        <h3>PHP</h3>
+                    </div>
+                    <div class="tech-card">
+                        <i class="bx bx-data icon-tech"></i>
+                        <h3>MySQL</h3>
+                    </div>
                 </div>
-                <div class="tech-card">
-                    <i class="bx bxl-css3 icon-tech"></i>
-                    <h3>CSS</h3>
-                </div>
-                <div class="tech-card">
-                    <i class="bx bxl-javascript icon-tech"></i>
-                    <h3>JavaScript</h3>
-                </div>
-                <div class="tech-card">
-                    <i class="bx bxl-php icon-tech"></i>
-                    <h3>PHP</h3>
-                </div>
-                <div class="tech-card">
-                    <i class="bx bx-data icon-tech"></i>
-                    <h3>MySQL</h3>
-                </div>
-            </div>
-        </section>
+            </section>
+        </main>
     </div>
 <script src="https://unpkg.com/lucide@latest"></script>
 <script>
