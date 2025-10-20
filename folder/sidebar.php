68ce23486ca4044ffa381e0794accd771a58c782
fikriaja1028 <fikriamazing5757@gmail.com>
2025-10-20 16:05:41 +0700

    sidbar

--- folder/sidebar.php
+++ folder/sidebar.php
@@ -5,10 +5,10 @@
             <a href="about.php" id="admin">Admin<span id="adminSpan">Panel<span></a>
         </div>
         <nav class="sidebar-link-container">
-            <a href="../pages/dashboard.php" class="side-link">Dashboard</a>
-            <a href="#" class="side-link">Tables</a>
-            <a href="../pages/settings.php" class="side-link">Settings</a>
-            <a href="../pages/about.php" class="side-link">About</a>
-            <a href="../pages/contact.php" class="side-link">Contact</a>
+            <a href="../pages/dashboard.php" class="side-link <?= basename(__FILE__) == 'dashboard.php' ? 'side-active' : '' ?>">Dashboard</a>
+            <a href="#" class="side-link <?= basename(__FILE__) == 'tables.php' ? 'side-active' : '' ?>">Tables</a>
+            <a href="../pages/settings.php" class="side-link <?= basename(__FILE__) == 'settings.php' ? 'side-active' : '' ?>">Settings</a>
+            <a href="../pages/about.php" class="side-link <?= basename(__FILE__) == 'about.php' ? 'side-active' : '' ?>">About</a>
+            <a href="../pages/contact.php" class="side-link <?= basename(__FILE__) == 'contact.php' ? 'side-active' : '' ?>">Contact</a>
         </nav>
     </aside>
