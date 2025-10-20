4f099996e76a6afd2cf0d62d0e2cab0f7d934ed8
XyStar123123 <hermawanbintang20@gmail.com>
2025-10-20 14:29:17 +0700

    Added changes to the sidebar.php (changing class name invention)

--- folder/sidebar.php
+++ folder/sidebar.php
@@ -4,11 +4,11 @@
             <img src="../img/icon.png" class="icon" style="width: 60px;">
             <a href="about.php" id="admin">Admin<span id="adminSpan">Panel<span></a>
         </div>
-        <nav class="nav_bar">
-            <a href="../pages/dashboard.php" class="nav_a">Dashboard</a>
-            <a href="#" class="nav_a">Tables</a>
-            <a href="../pages/settings.php" class="nav_a">Settings</a>
-            <a href="../pages/about.php" class="nav_a">About</a>
-            <a href="../pages/contact.php" class="nav_a">Contact</a>
+        <nav class="sidebar-link-container">
+            <a href="../pages/dashboard.php" class="side-link">Dashboard</a>
+            <a href="#" class="side-link">Tables</a>
+            <a href="../pages/settings.php" class="side-link">Settings</a>
+            <a href="../pages/about.php" class="side-link">About</a>
+            <a href="../pages/contact.php" class="side-link">Contact</a>
         </nav>
     </aside>
