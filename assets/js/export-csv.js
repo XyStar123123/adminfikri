6181f58666fe90affff454cfd401d07aa8537457
XyStar123123 <hermawanbintang20@gmail.com>
2025-10-23 10:55:33 +0700

    Fixed cdn boxicons

--- assets/js/export-csv.js
+++ assets/js/export-csv.js
@@ -1,6 +1,8 @@
 export function initExportCsv(){
     const convertCsvBtn = document.getElementById("convertCsvBtn")
 
+    if(!convertCsvBtn) return
+
     convertCsvBtn.addEventListener("click", ()=>{
         window.location.href = '../actions/export_products_csv.php';
     })
